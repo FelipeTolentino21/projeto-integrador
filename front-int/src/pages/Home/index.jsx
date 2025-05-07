e807fb6d23a18d224acd9ebef83e925fe1f7efb6
Rodrigo Everton <rodrigoeverton1500@gmail.com>
2025-05-07 14:50:23 -0300

    /home: adicionado header com logo, grid e cards

--- front-int/src/pages/Home/index.jsx
+++ front-int/src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
 import React, {useState} from "react";
-import {Container, Header} from "./styles";
+import {Container, Header, Board, Card, CardDescription, CardTitle, AddButton} from "./styles";
+import logo from "../../assets/logo-ceub.webp";
 
 export default function Home() {
     return (
         <Container>
             <Header>
-                ola
+                <img src={logo} alt="" />
             </Header>
+            <Board>
+                <Card>
+                    <CardTitle>Minicurso Java</CardTitle>
+                    <CardDescription>Lorem ipsum dolor, sit amet 
+                        consectetur adipisicing elit. 
+                        Iusto, repellat rem. Laboriosam esse id 
+                        aliquam sunt, corporis temporibus, 
+                        a dolore necessitatibus ex dolor 
+                        laborum velit iusto possimus veniam 
+                        dignissimos quisquam?</CardDescription>
+                </Card>
+                <AddButton>+</AddButton>
+            </Board>
         </Container>
     );
 }
--- front-int/src/pages/Home/styles.js
+++ front-int/src/pages/Home/styles.js
@@ -3,17 +3,88 @@ import {Link} from "react-router-dom";
 import "@fontsource/inter";
 
 export const Header = styled.div`
-    padding: 1rem;
+    background-color: #42154E;
+    color: white;
+    display: flex;
+    padding: 1rem 2rem;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 1.5rem;
+
+    img {
+        width: 100px;
+        height: auto;
+        background-color: white;
+        border-radius: 10%;
+        padding: 0.5rem;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    div {
+        display: flex;
+        gap: 1rem;
+    }
 `;
 
 export const Container = styled.div`
     text-align: center;
-    margin-top: 2rem;
-    font-family: "'Inter', sans-serif"
+    //margin-top: 2rem;
+    //padding: 1rem;
+    font-family: "Inter", sans-serif;
 `;
 
 export const Board = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 1rem;
+`;
+
+export const Card = styled.div`
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 1rem;
+    text-align: center;
     
+    p {
+        margin: 0.5rem 0 0;
+        font-size: 0.9rem;
+        color: gray;
+    }
+`;
+
+export const CardTitle = styled.h2`
+    fonts-size: 1.2rem;
+    margin: 0 0 0.5rem;
+    color: #42154E;
+`;
+
+export const CardDescription = styled.p`
+    font-size: 0.9rem;
+    color: black;
+    margin: 0 0 0.5rem;
+`;
+
+export const AddButton = styled.button`
+    background-color: #42154E;
+    color: white;
+    font-size: 2rem;
+    border: none;
+    border-radius: 50%;
+    width 20px;
+    height: 20px
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    margin: auto; 
+
+    &:hover {
+        background-color: #43054E;
+    }
 `;
 
 // import "@fontsource/inter";
